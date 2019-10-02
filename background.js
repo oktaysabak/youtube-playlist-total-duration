572717305f541468ea64d09ed3d9c463cfba0451
oktaysabak <oktaysabak@yandex.com>
2019-10-02 19:30:54 +0300

    it works

--- background.js
+++ background.js
@@ -1,3 +1,13 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    chrome.tabs.executeScript(null,{file:"yptd.js"});
-});
+      chrome.windows.getAll({populate:true},function(windows){
+    windows.forEach(function(window){
+      window.tabs.forEach(function(tab){
+        var url = tab.url;
+        if (url.includes("youtube")){
+            chrome.tabs.executeScript(null,{file:"yptd.js"});
+        }
+      });
+    });
+  });
+}); 
+
